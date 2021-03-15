3d577fec65065a510ac255d085b0f62f7c9fa030
Dean <qlqldldh@fadutec.com>
2021-03-16 00:17:23 +0900

    feat: add functions to update a post

--- sql_app/crud.py
+++ sql_app/crud.py
@@ -1,6 +1,7 @@
 from sql_app import models
 from sql_app.database import Session
-from sql_app.schemas import PostCreate
+from sql_app.schemas import PostCreate, Post
+from utils import get_dict_from_model
 
 
 def create_post(db: Session, post: PostCreate):
@@ -19,3 +20,14 @@ def get_post(db: Session, post_id: int):
 
 def get_post_list(db: Session):
     return db.query(models.Post).all()
+
+
+def update_post(db: Session, post: Post):
+    db_post = db.query(models.Post).get(post.id)
+    post_dict = get_dict_from_model(post)
+    for key, value in post_dict.items():
+        setattr(db_post, key, value)
+    db.commit()
+    db.refresh(db_post)
+
+    return db_post
--- sql_app/main.py
+++ sql_app/main.py
@@ -23,7 +23,7 @@ async def new_post(title: str, content: str, db: Session = Depends(get_db)):
     return crud.create_post(db, post)
 
 
-@app.get("/posts/", status_code=status.HTTP_200_OK)
+@app.get("/posts", status_code=status.HTTP_200_OK)
 def post_list(db: Session = Depends(get_db)):
     return crud.get_post_list(db)
 
@@ -31,3 +31,11 @@ def post_list(db: Session = Depends(get_db)):
 @app.get("/posts/{post_id}")
 def post_detail(post_id: int, db: Session = Depends(get_db)):
     return crud.get_post(db, post_id)
+
+
+@app.put("/posts/{post_id}/")
+def update_post(post_id: int, post: schemas.Post, db: Session = Depends(get_db)):
+    if post.id != post_id:
+        return {"message": "cannot update id of post"}
+
+    return crud.update_post(db, post)
